54829a59317a5aaa8333c7f42b9a6c1596934def
s20team <salehfffaaa@gmail.com>
2016-07-20 20:56:58 +0430

    Create stfbot.lua

---
+++ stfbot.lua
@@ -0,0 +1,24 @@
+  return [[ stf_bot
+〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+Anti spam Bot fOR Super groups
+〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+@h4ck_iran is #channel
+〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+@decopaje is #deVeLoper
+〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+Bot number : +در دسترس نیست
+〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+Bot version : 2.0 ]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "version: Shows bot version",
+  patterns = {
+    "^[#!/]version$",
+    "^stf_bot$"
+  }, 
+  run = run 
+}
+
+end
