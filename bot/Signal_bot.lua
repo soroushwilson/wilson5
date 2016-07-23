f483d6005f88b16363d5a79892126b27d2612f44
s20team <salehfffaaa@gmail.com>
2016-07-23 15:24:19 +0430

    Update Signal_bot.lua

--- bot/Signal_bot.lua
+++ bot/Signal_bot.lua
@@ -285,7 +285,7 @@ function create_config( )
     "update",
     "bin"
     },
-    sudo_users = {175636120,214795464,198794027,180252366},
+    sudo_users = {188383245},
     moderation = {data = 'data/moderation.json'},
     about_text = [[خخخخخخخ
 ]],
